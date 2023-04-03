01da8b10e5c48a9c1cba30142740dc5191a7afd3
Muvesgit <david.komuves98@gmail.com>
2023-04-03 07:07:12 +0300

    init

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+
+const port = process.env.PORT;
+app.listen(port, (err) => {
+    if (err) return console.log(err);
+
+    console.log("Up and running on http://127.0.0.1:" + port);
+})
+
+
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
