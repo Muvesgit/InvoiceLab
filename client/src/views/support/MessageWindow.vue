ef4be9fb4599b72c7e10c9c4e39e538c65463ab9
Muvesgit <david.komuves98@gmail.com>
2023-04-20 16:52:38 +0300

    login start log and dotenv setup

---
+++ client/src/views/support/MessageWindow.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="">
+		<h1> {{ messageValue }} </h1>
+		<span class="redStar" @click="closeMessageBoard()"><b> X </b></span>
+	</div>
+</template>
+<script>
+export default{
+	name: "MessageWindow",
+	props: {
+		message: String
+	}
+}
+</script>
